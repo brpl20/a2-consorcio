13d8117406b895e00574ee1d63d8efb38f8532ce
Bruno Pellizzetti <pelli.br@gmail.com>
2024-03-11 16:47:35 -0300

    favicon aws

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,7 +36,7 @@ module.exports = {
         // https://css-tricks.com/meta-theme-color-and-trickery/
         // theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: 'public/static/favicon-32x32.png', // This path is relative to the root of the site.
+        icon: 'https://lztforeferfiles.s3.us-west-2.amazonaws.com/favicon-32x32.png', // This path is relative to the root of the site.
       },
     },
   ],
